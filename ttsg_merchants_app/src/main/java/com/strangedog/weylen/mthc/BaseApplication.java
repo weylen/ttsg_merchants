ad9525ef1456d46c81f1c0f3c6e946df54f97a76
weylen <351531626@qq.com>
2016-09-02 13:27:38 +0700

    tuisong

--- ttsg_merchants_app/src/main/java/com/strangedog/weylen/mthc/BaseApplication.java
+++ ttsg_merchants_app/src/main/java/com/strangedog/weylen/mthc/BaseApplication.java
@@ -1,18 +1,100 @@
 package com.strangedog.weylen.mthc;
 
+import android.app.ActivityManager;
 import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Process;
+import android.util.Log;
 
 import com.rey.material.app.ThemeManager;
+import com.xiaomi.channel.commonutils.logger.LoggerInterface;
+import com.xiaomi.mipush.sdk.Logger;
+import com.xiaomi.mipush.sdk.MiPushClient;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2016-06-17.
  */
 public class BaseApplication extends Application{
 
+    // user your appid the key.
+    private static final String APP_ID = "2882303761517505999";
+    // user your appid the key.
+    private static final String APP_KEY = "5621750569999";
+
+    // 此TAG在adb logcat中检索自己所需要的信息， 只需在命令行终端输入 adb logcat | grep
+    // com.xiaomi.mipushdemo
+    public static final String TAG = "zhou";
+
+    private static DemoHandler sHandler = null;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         ThemeManager.init(this, 2, 0, null);
+
+        // 注册push服务，注册成功后会向DemoMessageReceiver发送广播
+        // 可以从DemoMessageReceiver的onCommandResult方法中MiPushCommandMessage对象参数中获取注册信息
+        if (shouldInit()) {
+            MiPushClient.registerPush(this, APP_ID, APP_KEY);
+        }
+
+        LoggerInterface newLogger = new LoggerInterface() {
+
+            @Override
+            public void setTag(String tag) {
+                // ignore
+            }
+
+            @Override
+            public void log(String content, Throwable t) {
+                Log.d(TAG, content, t);
+            }
+
+            @Override
+            public void log(String content) {
+                Log.d(TAG, content);
+            }
+        };
+        Logger.setLogger(this, newLogger);
+        if (sHandler == null) {
+            sHandler = new DemoHandler(getApplicationContext());
+        }
+    }
+
+    private boolean shouldInit() {
+        ActivityManager am = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE));
+        List<ActivityManager.RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
+        String mainProcessName = getPackageName();
+        int myPid = Process.myPid();
+        for (ActivityManager.RunningAppProcessInfo info : processInfos) {
+            if (info.pid == myPid && mainProcessName.equals(info.processName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static DemoHandler getHandler() {
+        return sHandler;
+    }
+
+    public static class DemoHandler extends Handler {
+
+        private Context context;
+
+        public DemoHandler(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            String s = (String) msg.obj;
+
+        }
     }
 }
